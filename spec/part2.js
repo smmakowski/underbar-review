909700195daa3dbb115a6ac5be7a5af2f54cc344
Stephen Makowski <makowski.sm@gmail.com>
2017-02-07 19:59:49 -0800

    "Uploaded from local directory"

--- spec/part2.js
+++ spec/part2.js
@@ -93,7 +93,7 @@
       it('fails for a collection of all-falsy values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.'); ////************
+        expect(_.every([false, 0, null], _.identity)).to.be.false;
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
@@ -151,7 +151,7 @@
       it('should fail for a set containing no matching values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.some([0, null, false], _.identity)).to.be.false;
       });
 
       it('should pass for a collection containing one matching value', function() {
@@ -191,7 +191,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {a: 'a'};
+        var source = {a: 'b'};
+        var extended = _.extend(destination, source);
+
+        expect(extended.a).to.equal('b');
       });
 
       it('should not override properties not found in the source', function() {
@@ -269,7 +273,13 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+       
+        var destination = {a: 1};
+        var source = {b: 2};
+
+        _.defaults(destination, source);
+
+        expect(destination.b).to.equal(2); 
       });
 
       it('should not copy a property if that key is already set on the target', function() {
